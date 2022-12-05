6aeb77ea52405b8a273cff58e5bc9dcfdb9c6ce5
tanzeelahmed2901 <tanzeel.ahmed@accenture.com>
2022-12-05 10:54:54 +0000

    arrow functions

--- javascript-functional-programming-1/exercises/arrow-functions.js
+++ javascript-functional-programming-1/exercises/arrow-functions.js
@@ -3,23 +3,26 @@
 
 //Rewrite this function in Arrow form:
 
-function sayHello(firstName, lastName) {
-    console.log(`sayHello: Hello ${firstName} ${lastName}`)
-}
+// function sayHello(firstName, lastName) {
+//     console.log(`sayHello: Hello ${firstName} ${lastName}`)
+// }
+// sayHello('Tom','Carabine')
+
+sayHello = (firstName, lastName) => console.log(`sayHello: Hello ${firstName} ${lastName}`)
 sayHello('Tom','Carabine')
 
 //TODO make hiEveryone()
 
-hiEveryone('Tom','Carabine')
+// hiEveryone('Tom','Carabine')
 
 
 // Rewrite this function in one-line terse arrow syntax:
 
-function loadsaMoney(myWages) {
-    return myWages * 10
-}
+loadsaMoney = (myWages) => {return myWages * 10}
+
+console.log(loadsaMoney(10))
 
 //TODO make lotsofMoney()
 
-const resultSum = lotsofMoney(300)
-console.log(`resultSum: ${resultSum}`)
+const resultSum = (money) => {return money * 100}
+console.log(`resultSum: ${resultSum(300)}`)
